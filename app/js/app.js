0c3c3de8bbf8a0b4db2409955466637499556fbc
Max Mautner <max.mautner@gmail.com>
2015-09-09 01:13:28 -0700

    initial commit

---
+++ app/js/app.js
@@ -0,0 +1,78 @@
+var app = angular.module('ghjobs', [
+  'ui.router',
+  'ngResource',
+  'angular-google-gapi'
+]);
+
+app.constant('BaseGHUrl', 'https://api.github.com')
+
+app.config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+      .state('base', {
+        abstract: true,
+        templateUrl: '/views/base.html'
+      })
+      .state('home', {
+        url: "/",
+        parent: 'base',
+        templateUrl: "views/home.html",
+        controller: "HomeCtrl"
+      })
+      .state('terms', {
+        url: "/terms",
+        parent: 'base',
+        templateUrl: "views/terms.html"
+      })
+      .state('orgs', {
+        url: "/org/:org",
+        parent: 'base',
+        templateUrl: "views/org.html",
+        controller: "OrgCtrl"
+      })
+      .state('repos', {
+        url: "/repos",
+        parent: 'base',
+        templateUrl: "views/repos.html",
+        controller: "RepoCtrl"
+      })
+      .state('events', {
+        url: "/events",
+        parent: 'base',
+        templateUrl: "views/events.html",
+        controller: "EventCtrl"
+      });
+  }
+]);
+
+
+app.service('Orgs', ['$resource', 'BaseGHUrl', function($resource, BaseGHUrl) {
+  return $resource(BaseGHUrl + '/organizations');
+}]);
+app.service('Org', ['$resource', 'BaseGHUrl', function($resource, BaseGHUrl) {
+  return $resource(BaseGHUrl + '/orgs/:org');
+}]);
+app.service('Repo', ['$resource', 'BaseGHUrl', function($resource, BaseGHUrl) {
+  return $resource(BaseGHUrl + '/search/repositories');
+}]);
+app.service('Event', ['$resource', 'BaseGHUrl', function($resource, BaseGHUrl) {
+  return $resource(BaseGHUrl + '/events');
+}]);
+app.service('Commit', ['$resource', 'BaseGHUrl', function($resource, BaseGHUrl) {
+  return $resource(BaseGHUrl + '/repos/:owner/:repo/commits/:sha');
+}]);
+
+app.controller('HomeCtrl', function(){});
+app.controller('OrgCtrl', ['$scope', '$stateParams', 'Orgs',
+  function($scope, $stateParams, Orgs) {
+  $scope.orgs = Orgs.query();
+}]);
+app.controller('RepoCtrl', ['$scope', '$stateParams', 'Repo',
+  function($scope, $stateParams, Repo) {
+  $scope.repos = Repo.get({q: "language=python"});
+}]);
+app.controller('EventCtrl', ['$scope', '$stateParams', 'Event',
+  function($scope, $stateParams, Event) {
+  $scope.events = Event.query();
+}]);
